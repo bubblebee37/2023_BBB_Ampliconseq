0281d3f2a547fd9fc636ea23a74f6db886906470
KYUNGHA KIM <67261077+bubblebee37@users.noreply.github.com>
2021-03-10 18:22:22 +0900

    Update SAMfile_using_bowtie2.sh

--- SAMfile_using_bowtie2.sh
+++ SAMfile_using_bowtie2.sh
@@ -1,4 +1,6 @@
 #Trimming 후 bowtie2를 사용해서 sequence alignment 정보를 담은 SAM 파일로 변경함
+#PhageM13 amplicon fasta 파일로 bowtie2를 사용해 indexing을 하고 나서 수행할 것 (.1.bt2 ~ .4.bt2, .rev.1.bt2, .rev.2.bt2 파일이 생성되야 함)
+
 for FQ1 in $(ls *_trim_1P)
 do
   FQ2=${FQ1/_trim_1P/_trim_2P}
